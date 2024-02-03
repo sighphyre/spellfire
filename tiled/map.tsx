954a1f2b87592c321a20a6c7d40502d3a3b5dc15
Dimitar Dobrev <dvdobrev.za@gmail.com>
2024-02-03 18:04:12 +0200

    tsx fix

---
+++ tiled/map.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="map" tilewidth="32" tileheight="32" tilecount="1344" columns="32">
+ <image source="../map.png" width="1024" height="1344"/>
+</tileset>
